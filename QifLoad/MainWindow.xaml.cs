15c4e1d06266c0cab70d4980eed5ef5f15904113
MikeAinOz <mike@mikeallen.com.au>
2019-05-05 12:44:47 +1000

    Add project files.

---
+++ QifLoad/MainWindow.xaml.cs
@@ -0,0 +1,151 @@
+﻿using QifApi;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QifLoad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public QifTransaction Qt;
+        public DataTable Dt;
+        
+        public MainWindow()
+            
+        {
+            
+            InitializeComponent();
+        }
+
+        private void LoadQifFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+
+            dlg.DefaultExt = ".qif";
+
+            dlg.Filter = "QIF Files (.qif)|*.qif";
+
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+
+            // Get the selected file name and display in a TextBox 
+
+            if (result == true)
+            {
+
+                // Open document 
+
+                string filename = dlg.FileName;
+
+                FileNameTextBox.Text = filename;
+
+                String LoadedMessage = "Loaded";
+                QifDom Qd = new QifDom();
+                //Qd.Import("C:\\Users\\Mike\\Documents\\AccountingDatabase\\010544496_20140817.qif");
+                Qd.Import(filename);
+                LoadMessage.Text = LoadedMessage;
+                Qt = new QifTransaction(Qd);
+                Dt = Qt.GetTransactionCollection();
+                TransactionGrid.ItemsSource = Dt.DefaultView;
+            
+            }         
+          
+        }
+        private void ExitApplication_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.PropertyType == typeof(System.DateTime))
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "dd/MM/yyyy";
+            if (e.PropertyType == typeof(System.Decimal))
+            {
+                (e.Column as DataGridTextColumn).Binding.StringFormat = "0.00";
+                Style style = new Style(); // creates object of style class   
+                style.TargetType = typeof(DataGridCell); //sets target type as DataGrid cell   
+                Setter setterAlignment = new Setter(); // create objects of setter class   
+                setterAlignment.Property = DataGridCell.HorizontalAlignmentProperty;
+                setterAlignment.Value = HorizontalAlignment.Right;
+                style.Setters.Add(setterAlignment);
+                (e.Column as DataGridTextColumn).CellStyle = style;
+            }
+            
+        }
+
+        private void SaveAsCsv_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+
+            dlg.DefaultExt = ".csv";
+
+            dlg.Filter = "CSV Files (.csv)|*.csv";
+
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                StreamWriter sw = new StreamWriter(dlg.FileName);
+                for (int i = 0; i < Dt.Columns.Count; i++)
+                {
+                    sw.Write(Dt.Columns[i]);
+                    if (i < Dt.Columns.Count - 1)
+                        sw.Write(',');
+                    else
+                        sw.Write('\n');
+                }
+                foreach (DataRow dr in Dt.Rows)
+                {
+                    for (int i = 0; i < Dt.Columns.Count; i++)
+                    {
+                        sw.Write( dr[i]);
+                        if (i < Dt.Columns.Count - 1)
+                            sw.Write(',');
+                        else
+                            sw.Write('\n');
+                    }
+                }
+                sw.Flush();
+                sw.Close();
+            }
+
+        }
+    }
+}
---
+++ QifLoad/QifTransaction.cs
@@ -0,0 +1,53 @@
+﻿using QifApi;
+using QifApi.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace QifLoad
+{
+    public class QifTransaction
+        {
+        QifDom QT_Dom = new QifDom();
+        public QifTransaction (QifDom qd)
+        {
+            QT_Dom = qd;
+        }
+        public DataTable GetTransactionCollection()
+        {
+            DataTable TransactionList = new DataTable();
+            TransactionList.Columns.Add("Date", typeof(DateTime));
+            TransactionList.Columns.Add("Amount", typeof(Decimal));
+        //    TransactionList.Columns.Add("Payee", typeof(String));
+            TransactionList.Columns.Add("Memo", typeof(String));
+          
+            StringBuilder MemoString = new StringBuilder();
+            MemoString.Clear();
+            foreach (BasicTransaction Bt in QT_Dom.BankTransactions)
+            {
+                if (MemoString.Length > 0)
+                {
+                    MemoString.Append(" ");
+                }
+                MemoString.Append(Bt.Memo);
+
+                if (Bt.Amount != 0)
+                {
+                    DataRow Dr = TransactionList.NewRow();
+                    Dr["Date"] = Bt.Date;
+                //    Dr["Payee"] = Bt.Payee;
+                    Dr["Amount"] = Bt.Amount;
+                    Dr["Memo"] = MemoString.ToString();
+
+                    TransactionList.Rows.Add(Dr);
+                    MemoString.Clear();
+                }
+            }
+            return TransactionList;
+        }
+    }
+}
